8e70be18e975154518d1e7a1b8f32f0c2bbaac3c
wurfmaul <wurfmaul@posteo.at>
2014-05-24 23:56:41 +0200

    insertion sort

--- src/at/jku/paugujooksik/client/sort/InsertionSort.java
+++ src/at/jku/paugujooksik/client/sort/InsertionSort.java
@@ -8,9 +8,22 @@ public class InsertionSort<T extends Comparable<T>> extends SortAlgorithm<T> {
 	public List<Action> getActions(List<T> values) {
 		final List<T> a = cloneList(values);
 		final int n = a.size();
-		
-		//TODO insertion sort
-		
+
+		for (int i = 1; i < n; i++) {
+			T x = a.get(i);
+			int j = i;
+			actions.add(Action.open(j, j - 1));
+			while (j > 0 && a.get(j - 1).compareTo(x) > 0) {
+				actions.add(Action.swap(j, j - 1));
+				a.set(j, a.get(j - 1));
+				j--;
+				if (j > 0)
+					actions.add(Action.open(j, j - 1));
+			}
+			a.set(j, x);
+		}
+
+		assert isListSorted(a);
 		return actions;
 	}
 
